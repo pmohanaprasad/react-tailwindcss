9a258035974e38f691943c2522131de744cad4f2
P Mohana Prasad <p.mohanaprasad@outlook.com>
2023-04-23 21:38:15 +0530

    init commit

---
+++ src/app/containers/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+
+import "./dashboardStyle.scss";
+
+const Dashboard = () => {
+  return (
+    <Container fluid className="dashboard p-0">
+      <Row>
+        <Col>Test</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Dashboard;
